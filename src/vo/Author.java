498519a77e0f09180aa7fe325051e84df2c2a214
RuicongSu <rs843@njit.edu>
2016-12-01 15:36:54 -0500

    Finish VO Author

--- src/vo/Author.java
+++ src/vo/Author.java
@@ -1,5 +1,22 @@
 package vo;
 
 public class Author {
+	private String auId;
+	private String auName;
 
+	public String getAuId() {
+		return auId;
+	}
+
+	public void setAuId(String auId) {
+		this.auId = auId;
+	}
+
+	public String getAuName() {
+		return auName;
+	}
+
+	public void setAuName(String auName) {
+		this.auName = auName;
+	}
 }
