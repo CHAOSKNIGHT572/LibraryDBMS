f6517c972c31fb877b7b93457f8fc0201196e3bf
RuicongSu <rs843@njit.edu>
2016-12-01 18:52:07 -0500

    Add and Finish VO - ConferenceProceeding

---
+++ src/vo/ConferenceProceeding.java
@@ -0,0 +1,31 @@
+package vo;
+
+public class ConferenceProceeding extends Document {
+	private String conDate;
+	private String conEditor;
+	private String conLocation;
+
+	public String getConDate() {
+		return conDate;
+	}
+
+	public void setConDate(String conDate) {
+		this.conDate = conDate;
+	}
+
+	public String getConEditor() {
+		return conEditor;
+	}
+
+	public void setConEditor(String conEditor) {
+		this.conEditor = conEditor;
+	}
+
+	public String getConLocation() {
+		return conLocation;
+	}
+
+	public void setConLocation(String conLocation) {
+		this.conLocation = conLocation;
+	}
+}
