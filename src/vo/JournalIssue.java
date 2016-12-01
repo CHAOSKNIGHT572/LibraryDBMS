771effe89155a4a6266d5bd6b63dcb6608f8ee70
RuicongSu <rs843@njit.edu>
2016-12-01 18:37:36 -0500

    Add and Finish VO - JournalIssue

---
+++ src/vo/JournalIssue.java
@@ -0,0 +1,31 @@
+package vo;
+
+public class JournalIssue {
+	private String issNum;
+	private String scope;
+	private String issEditor;
+
+	public String getIssNum() {
+		return issNum;
+	}
+
+	public void setIssNum(String issNum) {
+		this.issNum = issNum;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getIssEditor() {
+		return issEditor;
+	}
+
+	public void setIssEditor(String issEditor) {
+		this.issEditor = issEditor;
+	}
+}
