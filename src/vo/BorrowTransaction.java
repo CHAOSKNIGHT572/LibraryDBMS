99a5be9e3f1027da7ca1e0de0bf868721d34f09f
RuicongSu <rs843@njit.edu>
2016-12-01 16:48:41 -0500

    Finish VO BorrowTransaction

--- src/vo/BorrowTransaction.java
+++ src/vo/BorrowTransaction.java
@@ -1,5 +1,56 @@
 package vo;
 
 public class BorrowTransaction {
+	private String borNum;
+	private String borDateTime;
+	private String retDateTime;
+	private String dueDate;
+	private String fineAmount;
+	private Document document;
+	private Branch branch;
+	private Reader reader;
 
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+
+	public Reader getReader() {
+		return reader;
+	}
+
+	public void setReader(Reader reader) {
+		this.reader = reader;
+	}
+
+	public String getBorNum() {
+		return borNum;
+	}
+
+	public String getBorDateTime() {
+		return borDateTime;
+	}
+
+	public String getRetDateTime() {
+		return retDateTime;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public String getFineAmount() {
+		return fineAmount;
+	}
 }
