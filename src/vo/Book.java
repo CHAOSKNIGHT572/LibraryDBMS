92d17295f2ec77008cf2f72fbc1e1508df9dd6a7
RuicongSu <rs843@njit.edu>
2016-12-01 17:11:15 -0500

    Add and Finish VO - Book

---
+++ src/vo/Book.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class Book extends Document {
+	private String isbn;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+}
