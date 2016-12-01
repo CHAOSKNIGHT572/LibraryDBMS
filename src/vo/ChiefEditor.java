d296941b59fd64781cc839a86ad63ace93b3a8e9
RuicongSu <rs843@njit.edu>
2016-12-01 18:21:47 -0500

    Add and Finish VO - ChiefEditor

---
+++ src/vo/ChiefEditor.java
@@ -0,0 +1,18 @@
+package vo;
+
+public class ChiefEditor {
+	private String ceId;
+	private String ceName;
+	public String getCeId() {
+		return ceId;
+	}
+	public void setCeId(String ceId) {
+		this.ceId = ceId;
+	}
+	public String getCeName() {
+		return ceName;
+	}
+	public void setCeName(String ceName) {
+		this.ceName = ceName;
+	}
+}
