bdb92604cf8d62708da5ab44ba40e5bfe659f266
RuicongSu <rs843@njit.edu>
2016-12-01 17:09:12 -0500

    Finish VO - Reservation

--- src/vo/Reservation.java
+++ src/vo/Reservation.java
@@ -1,5 +1,46 @@
 package vo;
 
 public class Reservation {
+	private String resNum;
+	private String resDateTime;
+	private String resStatus;
+	private Document document;
+	private Branch branch;
+	private Reader reader;
 
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+
+	public Reader getReader() {
+		return reader;
+	}
+
+	public void setReader(Reader reader) {
+		this.reader = reader;
+	}
+
+	public String getResNum() {
+		return resNum;
+	}
+
+	public String getResDateTime() {
+		return resDateTime;
+	}
+
+	public String getResStatus() {
+		return resStatus;
+	}
 }
