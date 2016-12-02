9ff76cdae95aa90739197c159213414c70761947
RuicongSu <rs843@njit.edu>
2016-12-01 19:56:38 -0500

    Add and Finish VO - Copy

---
+++ src/vo/Copy.java
@@ -0,0 +1,32 @@
+package vo;
+
+public class Copy {
+	private String cpId;
+	private Document document;
+	private Branch branch;
+
+	public String getCpId() {
+		return cpId;
+	}
+
+	public void setCpId(String cpId) {
+		this.cpId = cpId;
+	}
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+
+}
