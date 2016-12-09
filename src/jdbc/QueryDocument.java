3e94ed46c9e2e9da6a829185b5aca5b56638489b
RuicongSu <rs843@njit.edu>
2016-12-09 10:51:10 -0500

    Modify - Query Document and Reader

--- src/jdbc/QueryDocument.java
+++ src/jdbc/QueryDocument.java
@@ -5,40 +5,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import vo.Author;
 import vo.Document;
-import vo.Publisher;
-import vo.Book;
 
 //[Query] regard to document
 
 public class QueryDocument {
+	private static final String GET_BOOK_BY_ISBN = "SELECT document.DocId, Title FROM document, book WHERE book.DocId = document.DocId AND ISBN = ?";
 
-	//get docid by type in isbn
-	public static ResultSet getBookInfo(Book book) {
-			Connection conn;
-			if ((conn = ConnectionBuilder.getConnection()) == null) {
-				return null;
-			}
-			PreparedStatement ps = null;
-			String sqlString = "";
-			if (book.getIsbn() != null) {
-				sqlString = "SELECT document.DocId, Title FROM document, book WHERE book.DocId = document.DocId AND ISBN = " + book.getIsbn();
-			}
-			System.out.println(sqlString);
-
-			ResultSet rs = null;
-			try {
-				ps = conn.prepareStatement(sqlString);
-				rs = ps.executeQuery();
-			} catch (SQLException e) {
-				e.printStackTrace();
-				return null;
-			}
-			return rs;
+	// get docid by type in isbn
+	public static ResultSet getBookByISBN(String isbn) {
+		Connection conn;
+		if ((conn = ConnectionBuilder.getConnection()) == null) {
+			return null;
+		}
+		ResultSet rs = null;
+		try {
+			PreparedStatement ps = conn.prepareStatement(GET_BOOK_BY_ISBN);
+			ps.setString(1, isbn);
+			rs = ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return rs;
 	}
-	
+
 	// get where documents are available by type in DocId
 	public static ResultSet getDocWhere(Document doc) {
 		Connection conn;
@@ -46,12 +38,12 @@ public class QueryDocument {
 			return null;
 		}
 		PreparedStatement ps = null;
-		
+
 		String sqlString = "";
 		if (doc.getId() != null) {
-		sqlString = "SELECT Title, copy.DocId, CopyNo, copy. Position, branch.Name, branch.Location" + 
-		" FROM (document JOIN copy ON document.DocId = copy.DocId) JOIN branch ON branch.LibId = copy.LibId WHERE document.DocId = 1" + 
-		" WHERE document.DocId = " + doc.getId();
+			sqlString = "SELECT Title, copy.DocId, CopyNo, copy. Position, branch.Name, branch.Location"
+					+ " FROM (document JOIN copy ON document.DocId = copy.DocId) JOIN branch ON branch.LibId = copy.LibId WHERE document.DocId = 1"
+					+ " WHERE document.DocId = " + doc.getId();
 		}
 		System.out.println(sqlString);
 
@@ -65,72 +57,55 @@ public class QueryDocument {
 		}
 		return rs;
 	}
-	
-	
-	
-//Get detailed document info
-	public static ResultSet getDocInfo(Document doc) {
+
+	// Get detailed document info
+	public static ResultSet getDocumentByMultiCondition(Document doc) {
 		Connection conn;
 		if ((conn = ConnectionBuilder.getConnection()) == null) {
 			return null;
 		}
 		PreparedStatement ps = null;
-		///**/
-		String selfm = "SELECT document.DocId, Title, Type, publisher.pubName, author.AName, PDate FROM document";
-		String where = " WHERE";
-		
-		if (doc.getPublisher() != null) { //if there is a publisher name
+		String selfm = "SELECT document.DocId, Title, Type, publisher.pubName, author.AName, PDate FROM document, publisher, author, writes";
+		String where = " WHERE document.PublisherId = publisher.PublisherId AND document.DocId = writes.DocId AND writes.AuthorId = author.AuthorId";
 
-			selfm += ", publisher";
-			where += " document.PublisherId = publisher.PublisherId AND publisher.PubName LIKE '%"
-					+ doc.getPublisher().getPubName() + "'% AND";
+		// if there is a publisher name
+		if (doc.getPublisher() != null) {
+			where += " AND PubName LIKE '%" + doc.getPublisher().getPubName() + "%'";
 		}
-
-		if (doc.getAuthorList().size() != 0) { // if there is a list of authors
+		// if there is a list of authors
+		if (doc.getAuthorList().size() != 0) {
 			String authors = "";
 			List<Author> authorslist = doc.getAuthorList();
 			for (Author author : authorslist) {
 				authors += "'" + author.getAuName() + "',";
 			}
-			String authorswoc = authors.substring(0, authors.length() - 1);
-
-			selfm += ", author, writes";
-			where += " author.AName IN (" + authorswoc + ") AND document.DocId = writes.DocId"
-					+ " AND writes.AuthorId = author.AuthorId AND";
-
+			where += " AND author.AName IN (" + authors.substring(0, authors.length() - 1) + ")";
 		}
-		if (doc.getDescriptorList().size() != 0) { //if there is a list of descriptor
+		// if there is a list of descriptor
+		if (doc.getDescriptorList().size() != 0) {
 			String descriptors = "";
 			List<String> descriptorslist = doc.getDescriptorList();
 			for (String descriptor : descriptorslist) {
-				descriptors += descriptor;
+				descriptors += "'" + descriptor + "',";
 			}
 			selfm += ", descriptor";
-			where += " D_value IN (" + descriptors + ") AND document.DocId = descriptor.DocId";
+			where += " AND D_value IN (" + descriptors.substring(0, descriptors.length() - 1)
+					+ ") AND document.DocId = descriptor.DocId";
 		}
-		if (doc.getId() != null) { //if there is a docid
-			where += " DocId = " + doc.getId() + " AND";
+		// if there is a docid
+		if (doc.getId() != null) {
+			where += " AND DocId = " + doc.getId();
 		}
-		if (doc.getTitle() != null) { //if there is a title
-			where += " Title = " + doc.getTitle() + " AND";
+		// if there is a title
+		if (doc.getTitle() != null) {
+			where += " AND Title = " + doc.getTitle();
 		}
-		if (doc.getPubDate() != null) { //if there is a publisher date
-			where += " PDate = " + doc.getPubDate() + " AND";
+		// if there is a publisher date
+		if (doc.getPubDate() != null) {
+			where += " AND PDate = " + doc.getPubDate();
 		}
-		
-		String sqlString = "";
-	
-		if (where.endsWith("AND") == true) { // if the sql statement ends with AND
-			where = where.substring(0, where.length() - 4);
-			sqlString = selfm + where;
-		}
-		else {
-			sqlString = selfm;
-		}
-		
-
+		String sqlString = selfm + where;
 		System.out.println(sqlString);
-
 		ResultSet rs = null;
 		try {
 			ps = conn.prepareStatement(sqlString);
@@ -140,25 +115,23 @@ public class QueryDocument {
 			return null;
 		}
 		return rs;
-	} 
-	//This is a test
+	}
+
+	// This is a test
 	public static void main(String[] args) {
-//		Publisher publisher = new Publisher();
-//		publisher.setPubName("RS");
-//		Author author = new Author();
-//		author.setAuName("");
-//		Document document = new Document();
-//		document.setPublisher(publisher);
-//		document.addAuthor(author);
-		Book book = new Book();
-		book.setIsbn("123456789");
-		
-		//ResultSet rs = getDocInfo(document);
-		ResultSet rs = getBookInfo(book);
-		
+		// Publisher publisher = new Publisher();
+		// publisher.setPubName("RS");
+		// Author author = new Author();
+		// author.setAuName("");
+		// Document document = new Document();
+		// document.setPublisher(publisher);
+		// document.addAuthor(author);
+		// ResultSet rs = getDocInfo(document);
+		ResultSet rs = getBookByISBN("123456789");
+
 		try {
-			while(rs.next()){
-				for(int i = 1; i < 1; ++i){
+			while (rs.next()) {
+				for (int i = 1; i < 1; ++i) {
 					System.out.print(rs.getString(i));
 					System.out.print(',');
 				}
--- src/jdbc/QueryReader.java
+++ src/jdbc/QueryReader.java
@@ -8,136 +8,73 @@ import java.sql.SQLException;
 import vo.Reader;
 
 public class QueryReader {
-	public static ResultSet readerFullInfo(Reader reader) {
+	private static final String GET_READER_BY_ID = "SELECT * FROM reader WHERE ReaderId = ?";
+	private static final String GET_READER_BY_NAME = "SELECT ReaderId, Address, Phone FROM reader WHERE ReaderName LIKE ?";
+	private static final String GET_READER_BY_PHONE = "SELECT ReaderId, Address, Phone FROM reader WHERE ReaderPhone = ?";
+
+	public static ResultSet getReaderById(String readerId) {
 		Connection conn;
 		if ((conn = ConnectionBuilder.getConnection()) == null) {
 			return null;
 		}
-		PreparedStatement ps = null;
-		
-
-		String sqlString = "";
-
-		if (reader.getId() != null && reader.getName() != null && reader.getPhoneNum() != null) { 
-			//Type in id, name and phone number, get full info about reader and borrow info.
-			sqlString = "SELECT bor_transaction.ReaderId, ReaderName, PhoneNum, BorNumber, bor_transaction.DocId, CopyNo, Title, branch.Name, Location" + 
-		", BorDateTime, RetDateTime, DueDate" + //SELECT
-			 " FROM ((bor_transaction JOIN reader ON bor_transaction.readerId = reader.readerId)" + 
-    			" JOIN document ON document.DocId = bor_transaction.DocId)" + 
-        			" JOIN branch ON branch.LibId = bor_transaction.LibId" + //FROM
-		" WHERE reader.ReaderId = " + reader.getId();//WHERE
-		}
-		else {
-			sqlString = "SELECT * FROM reader";
-		}
-
-		System.out.println(sqlString);
-
 		ResultSet rs = null;
 		try {
-			ps = conn.prepareStatement(sqlString);
+			PreparedStatement ps = conn.prepareStatement(GET_READER_BY_ID);
+			ps.setString(1, readerId);
 			rs = ps.executeQuery();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
 		}
 		return rs;
 	}
-	
 
-	
-	public static ResultSet readerBasicInfo(Reader reader) {
+	public static ResultSet getReaderByName(String name) {
 		Connection conn;
 		if ((conn = ConnectionBuilder.getConnection()) == null) {
 			return null;
 		}
-		PreparedStatement ps = null;
-		
-		String sel = "SELECT"; 
-		String fm = " FROM reader";
-		String where = " WHERE";
-		String sqlString = "";
-		if (reader.getId() == null && reader.getName() != null && reader.getPhoneNum() == null) { //Type in name, get phone# and reader id;
-			sel += " ReaderId, Address, PhoneNum";
-			where += " ReaderName = " + "'" + reader.getName() + "'";
-			sqlString = sel + fm + where;
-		}
-		else if (reader.getId() == null && reader.getName() == null && reader.getPhoneNum() != null) { //Type in phone#, get name and reader id;
-			sel += " ReaderId, ReaderName, Address";
-			where += " PhoneNum = " + reader.getPhoneNum();
-			sqlString = sel + fm + where;
-		}
-		else if (reader.getId() != null && reader.getName() == null && reader.getPhoneNum() == null){
-			sel += " *";
-			where += " ReaderId = " + reader.getId();
-			sqlString = sel + fm + where;
-		}
-		else {
-			sel += " *";
-			sqlString = sel + fm;
-		}
-		
-		
-		
-	/*	
-		if (where.endsWith("AND") == true) {
-			where = where.substring(0, where.length() - 4);
-			QUERY_STR_2 = sel + fm + where;
-		}
-		*/
-		System.out.println(sqlString);
-/*
-		try {
-			ps = conn.prepareStatement(QUERY_STR_2);
-			ps.setString(0, "sjdlfj");
-			ps.setInt(1, 4);
-			ResultSet rs = ps.executeQuery();
-
-		} catch (SQLException e) {
-			if (ps != null) {
-				System.out.println(ps);
-				System.out.println();
-			}
-		}
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			System.out.println("No Connection Started");
-		}
-		*/
 		ResultSet rs = null;
 		try {
-			ps = conn.prepareStatement(sqlString);
+			PreparedStatement ps = conn.prepareStatement(GET_READER_BY_NAME);
+			name = "%" + name + "%";
+			ps.setString(1, name);
 			rs = ps.executeQuery();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return null;
 		}
-		/*try {
-			conn.close();
-		} catch (SQLException e) {
-			System.out.println("No Connection Started");
-		}
-		*/
 		return rs;
 	}
-	
-	
-	
-//This is a test.
+
+	public static ResultSet getReaderByPhone(String phone) {
+		Connection conn;
+		if ((conn = ConnectionBuilder.getConnection()) == null) {
+			return null;
+		}
+		ResultSet rs = null;
+		try {
+			PreparedStatement ps = conn.prepareStatement(GET_READER_BY_PHONE);
+			ps.setString(1, phone);
+			rs = ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	// This is a test.
 	public static void main(String[] args) {
 		Reader reader = new Reader();
 		reader.setId("3");
 		reader.setName("RS");
-//		reader.setType("");
-//		reader.setAddress("");
+		// reader.setType("");
+		// reader.setAddress("");
 		reader.setPhoneNum("3333333333");
-//		
-//		ResultSet rs = readerBasicInfo(reader);
-		ResultSet rs = readerFullInfo(reader);
+		//
+		// ResultSet rs = readerBasicInfo(reader);
+		ResultSet rs = getReaderById("3");
 		try {
-			while(rs.next()){
-				for(int i = 1; i < 1; ++i){
+			while (rs.next()) {
+				for (int i = 1; i < 1; ++i) {
 					System.out.print(rs.getString(i));
 					System.out.print(',');
 				}
