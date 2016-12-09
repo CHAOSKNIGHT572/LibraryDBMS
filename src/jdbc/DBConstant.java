393a0ac19cb92cdbc6c97a41b5d7401a45d3a0e1
RuicongSu <rs843@njit.edu>
2016-12-09 09:59:41 -0500

    Modify - Database Connection Settings

--- src/jdbc/DBConstant.java
+++ src/jdbc/DBConstant.java
@@ -4,7 +4,5 @@ public class DBConstant {
 	public static final String USER = "root";
 	public static final String PASS = "root";
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	// public static final String PATH =
-	// "jdbc:mysql://localhost:3306/cs631?characterEncoding=utf8";
 	public static final String PATH = "jdbc:mysql://localhost:3306/cs631?useSSL=false&characterEncoding=utf8";
 }
